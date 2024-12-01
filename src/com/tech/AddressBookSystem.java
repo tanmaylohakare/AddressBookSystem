3e988edbac12ae4c9d1d0ac7d88e461d41eb89f9
tanmaylohakare <tanmaylohakare820@gmail.com>
2024-12-01 23:11:28 +0530

    Add details for 50 Student

---
+++ src/com/tech/AddressBookSystem.java
@@ -0,0 +1,115 @@
+package com.tech;
+
+public class AddressBookSystem {
+	
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String email;
+
+	
+	public AddressBookSystem(String firstName, String lastName, String address, String city, String state, String zip,
+			String phoneNumber, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "AddressBookSystem [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address
+				+ ", city=" + city + ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email="
+				+ email + "]";
+	}
+	
+	public void showinfo()
+	{
+		
+		System.out.println("Full Name :- "+firstName+" "+lastName);
+		System.out.println("Address :- "+ address);
+		System.out.println("City :- "+ city);
+		System.out.println("State :- "+ state);
+		System.out.println("zip :- "+zip);
+		System.out.println("Phone Number :- "+phoneNumber);
+		System.out.println("Emial :- "+email);
+		
+	}
+	
+}
+
+
--- src/com/tech/Main.java
+++ src/com/tech/Main.java
@@ -1,11 +1,53 @@
 package com.tech;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to the Address Book System");
 		
+
+        AddressBookSystem[] addressBookArray = new AddressBookSystem[50]; 
+	
+		Scanner sc = new Scanner(System.in);
+        int count =0;
+
+        for (int i = 0; i < 50; i++) {
+        	
+        	System.out.println("\n Enter details for Person "+ (i+1) + " :");
+		
+		System.out.print("Enter First Name: ");
+		String firstName = sc.nextLine();
+		
+		System.out.print("Enter Last Name: ");
+		String lastName = sc.nextLine();
+		
+		System.out.print("Enter Address: ");
+		String address = sc.nextLine();
+		
+		System.out.print("Enter City: ");
+		String city = sc.nextLine();
+		
+		System.out.print("Enter State: ");
+		String state = sc.nextLine();
+		
+		System.out.print("Enter ZIP: ");
+		String zip = sc.nextLine();
+		
+		System.out.print("Enter Phone Number: ");
+		String phoneNumber = sc.nextLine();
+		
+		System.out.print("Enter Email: ");
+		String email = sc.nextLine();
+		
+		
+        addressBookArray[i] = new AddressBookSystem(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        count ++;
+            }
+		
+	}
 	}
 
-}
+
